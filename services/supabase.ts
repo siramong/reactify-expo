b3e85fa645ef694806c0e490a890db154040c9da
estcraft08 <estcraft08@gmail.com>
2025-10-29 21:07:39 +0000

    ♻️ Refactor (supabase.ts): Cambio de Keys

--- services/supabase.ts
+++ services/supabase.ts
@@ -9,7 +9,8 @@ export type Coin = {
   created_at?: string;
 };
 
+// Usar la Secret API Key (Service Role Key) desde variables de entorno
 const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL!;
-const SUPABASE_ANON_KEY = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
+const SUPABASE_SECRET_KEY = process.env.EXPO_PUBLIC_SUPABASE_SECRET_KEY!; // <-- tu secret key
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_SECRET_KEY);
